8f45a6092378686166deceea21f9e130069c4e95
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2022-01-27 10:22:11 +0100

    still debugging

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const axios = require('axios')
 const app = express();
 
 app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
 
 const PUT = 'put', POST = 'post', GET = 'get', PATCH = 'patch', DELETE = 'delete'
 
